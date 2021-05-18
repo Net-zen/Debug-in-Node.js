5949041a36613d26758aa656cdc2627095963089
Net-zen <prankster@myttk.ru>
2021-05-18 12:46:01 +0600

    refactor: add port number to console.log in app.listen

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
@@ -10,6 +11,6 @@ app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
+app.listen(process.env.PORT, function() {
+    console.log(`App is listening on ${process.env.PORT}`);
 })
