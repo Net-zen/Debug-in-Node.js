3c914fad041138e94cd3936667b11338fd284e98
Net-zen <prankster@myttk.ru>
2021-05-20 20:21:33 +0600

    refactor: validate-session.js refactor to async/await functions, use try/catch constructions, add semicolons in right places

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -2,31 +2,29 @@ const jwt = require('jsonwebtoken');
 const DataTypes = require('sequelize');
 const sequelize = require('../db');
 
-const User = require('../models/user')(sequelize, DataTypes);
+const User = require("../models/user")(sequelize, DataTypes);
 
-module.exports = function (req, res, next) {
-    if (req.method === 'OPTIONS') {
-        next();   // allowing options as a method for request
-    } else {
-        const sessionToken = req.headers.authorization;
-        console.log(sessionToken);
-        if (!sessionToken) return res.status(403).send({ auth: false, message: "No token provided." });
-        else {
-            jwt.verify(sessionToken, 'lets_play_sum_games_man', (err, decoded) => {
-                if (decoded) {
-                    User.findOne({ where: { id: decoded.id } }).then(user => {
-                        req.user = user;
-                        console.log(`user: ${user}`)
-                        next()
-                    },
-                        function () {
-                            res.status(401).send({ error: "not authorized" });
-                        })
-
-                } else {
-                    res.status(400).send({ error: "not authorized" })
-                }
-            });
-        }
+module.exports = async (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    next(); // allowing options as a method for request
+  } else {
+    const sessionToken = req.headers.authorization;
+    if (!sessionToken)
+      return res
+        .status(403)
+        .send({ auth: false, message: "No token provided." });
+    else {
+      try {
+        const decoded = await jwt.verify(sessionToken, "lets_play_sum_games_man");
+          try {
+            req.user = await User.findOne({ where: { id: decoded.id } });
+            next();
+          } catch (err) {
+            res.status(401).send({ error: "not authorized" });
+          }
+      } catch (err) {
+        res.status(400).send({ error: "not authorized" });
+      }
     }
-}
+  }
+};
