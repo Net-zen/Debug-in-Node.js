07d50caace6b3df3ce97f990d68fcb72cd96112b
Net-zen <prankster@myttk.ru>
2021-05-20 22:27:03 +0600

    refactor: add logging: false in new Sequelize options to hide unnecessary db logs

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    operatorsAliases: false
+    operatorsAliases: false,
+    logging: false
 });
 
 sequelize.authenticate()
