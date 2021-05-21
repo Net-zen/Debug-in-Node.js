0efd52f87e5374a63a0bd2e5ce8226a1109ec881
Net-zen <prankster@myttk.ru>
2021-05-21 23:58:53 +0600

    refactor: anonymous function replaced with arrow function

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use(express.urlencoded({
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT, () => {
     console.log(`App is listening on ${process.env.PORT}`);
 });
